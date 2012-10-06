f24d52475a7a44e7e530a812aae1aed9847e807f
Luke Strickland <luke@axxim.net>
2012-10-05 20:42:45 -0500

    Maybe fixed Travis?

--- .scripts/mongo-php-driver-installer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-wget http://pecl.php.net/get/mongo-2.2.0.tgz
-tar -xzf mongo-2.2.0.tgz
-sh -c "cd mongo-2.2.0 && phpize && ./configure && sudo make install"
-echo "extension=mongo.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
